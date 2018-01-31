daf43a8457bcc8683319c963dfcc289c5026aa67
Majick Tek <kingtmw2@gmail.com>
2018-01-31 11:16:27 -0500

    Create Cleanup.java

---
+++ Map Editor/utils/Cleanup.java
@@ -0,0 +1,33 @@
+//Cleans up CSV files exported from Tiled
+
+import java.io.*;
+
+public class Cleanup
+    {
+     public static void main(String args[])
+         {
+         try
+             {
+             File file = new File(args[1]);
+             BufferedReader reader = new BufferedReader(new FileReader(file));
+             String line = "", oldtext = "";
+             while((line = reader.readLine()) != null)
+                 {
+                 oldtext += line + "\r\n";
+             }
+             reader.close();
+             // replace a word in a file
+             String newtext = oldtext.replaceAll("-1", "0"); // replace -1 with 0
+            
+             //To replace a line in a file
+             //String newtext = oldtext.replaceAll("This is test string 20000", "blah blah blah");
+            
+             FileWriter writer = new FileWriter(args[1]);
+             writer.write(newtext);writer.close();
+         }
+         catch (IOException ioe)
+             {
+             ioe.printStackTrace();
+         }
+     }
+}
